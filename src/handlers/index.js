8dc0d985f2301da2ff774b393df5a2a7536a74f8
Styop0909 <stepan.grig.2009@gmail.com>
2026-07-22 16:49:28 +0400

    Update index.js

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,7 +1,1 @@
-export { default as UserHandlers } from './user.js';
-export { default as OrderHandlers } from './order.js';
-export { default as BonusHandlers } from './bonus.js';
-export { default as PartnerHandlers } from './partner.js';
-export { default as FastChargeHandlers } from './fastcharge.js';
-export { default as AdminHandlers } from './admin.js';
-export { default as MenuHandlers } from './menu.js';
+// Empty - all handlers are in admin.js
